c1a5a53ea921a1f9e5f5e6b80a92057a0c44c786
1heidi <oneheidihere@gmail.com>
2024-07-22 12:26:57 -0500

    updating data, scripts, and figures

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Update of the GBC Biodata Inventory
 
 ## Work-In-Progress!
-## Testing ML pipeline, manual review, and post-review processing
-### See associated repo: https://github.com/globalbiodata/inventory_2022
 
-* ** Have completed pipeline via Google Colan and updating_inventory.ipynb
-* ** Currently augmenting country extraction for author affilicaiton and URLs for better coverage
-* ** To do: Anlayze changes in citations and URL status for resources indentified in the 2022 inventory
+### Starting with orginal repo: https://github.com/globalbiodata/inventory_2022
+* Repeated ML pipeline via updating_inventory.ipyn for publication years 2022 and 2023
+* Manually reviewed flagged results and completed post-processing pipeline
+
+### Analyze countries for new resources via:
+  * Author affiliation
+  * URL geocoordinates
+  * Both known to be tricky; augmented with additional packages/scripts for better coverage
+  * Notes on debatable decisions: 
+    * 1) Hong Kong left separate from China, but Guadeloupe combined with France
+    * 2) Augmenting means that provision of input files and scripts allows for reproducibility but scripts will not be generalizable for future updates! Data and code must be reviewed/edited to ensure new countries are not missed, etc. 
+  * See preliminary maps (jpegs)
+  
+### To do: Analyze updated metadata for resources identified in 2022 inventory:
+  * e.g., changes in citations and URL status
--- STEP_2_analyze_new_resources.R
+++ STEP_2_analyze_new_resources.R
@@ -1,8 +1,9 @@
 ## STEP 2 - Analyze Updated Inventory
-## Purpose: 
+## Purpose: Analyze metadata for biodata resources identified in publication years 2022 and 2023.
+## Parts: 1) Isolate Newly ID'd Resources, 2) Augment Country from Author Affiliations, 3) Augment Country from URL Geo-coordinates, and 4) Plot Countries for Newly ID'd Resources  
 ## Package(s): tidyverse, countrycode, maps, ggplot2, ggmap
-## Input file(s): predictions_final_2024-07-12.csv, final_inventory_2022.csv; scripts based on those written by Ken Schackart at https://github.com/globalbiodata/inventory_2022/blob/main/analysis/location_information.R
-## Output file(s): url_countries_new_resources_2024.jpeg, 
+## Input file(s): predictions_final_2024-07-12.csv, final_inventory_2022.csv; scripts are based on those written by Ken Schackart at https://github.com/globalbiodata/inventory_2022/blob/main/analysis/location_information.R
+## Output file(s): new_biodata_resources_2024_unaugmented.csv, new_biodata_resources_2024_augmented.csv, url_countries_new_resources_2024.jpeg, 
 
 library(tidyverse)
 library(countrycode)
@@ -10,14 +11,14 @@ library(maps)
 library(ggplot2)
 library(ggmap)
 
-i <- read.csv("final_inventory_2022.csv")
-ui <- read.csv("predictions_final_2024-07-12.csv")
-
 ##==================================================##
 ######### PART 1: Isolate Newly ID'd Resources ####### 
 ##==================================================##
 
 ## Changes in the IDs (e.g., for those with new publications in 2022/23), URL status, and metadata retrieved from Europe PMC (e.g., citation counts) prevents filtering by an anti_join. Additionally, filtering by just publication date returns resources from the 2022 inventory which have had another paper published in 2022 or 2023 since publication_date lists the newest. So, to find those new to this 2024 inventory update, select just best_name, ID, and publication date to match on best_name, then double check dates.
+
+i <- read.csv("final_inventory_2022.csv")
+ui <- read.csv("predictions_final_2024-07-12.csv")
 
 i2 <- select(i, 1, 14, 2)
 i2$publication_date <- as.Date(i2$publication_date) 
@@ -46,75 +47,78 @@ new <- select(new, -ends_with(".y"))
 #     ignore_case = TRUE,
 #     distance_col = 'distance')
 
-write.csv(new, "new_biodata_resources_2024.csv", row.names = FALSE)
+write.csv(new, "new_biodata_resources_2024_unaugmented.csv", row.names = FALSE)
 
 ##==============================================================##
 ######### PART 2: Augment Country from Author Affiliations ####### 
 ##==============================================================##
 
-## 66 rows empty for author affiliation_countries (~10%); attempt to extract additional countries from affiliation using another package
+## Missing Affiliation Countries: 66 rows empty for author affiliation_countries (~10%); attempt to extract additional countries from affiliation using another package
 
 ## patterns to detect
 all_country <- countrycode::countryname_dict %>% 
   filter(grepl('[A-Za-z]', country.name.alt)) %>%
   pull(country.name.alt) 
-## above still does not catch UK, Iran, or just "Korea" (?!) - add manually
+## above still does not catch UK, Iran, and just "Korea" (?!) - add manually
 all_country <- c(all_country, "UK", "Iran", "Korea")
 pattern <- str_c(all_country, collapse = '|')
 
-## don't want to replace those found via the pipeline, so filter to just those with missing affiliations and string detect
+## don't want to replace those found via the pipeline, so filter to just those with missing affiliations to detect strings
 
-missing <- filter(new, new$affiliation_countries == "")
+mac <- filter(new, new$affiliation_countries == "")
 
-missing <- missing %>% 
+mac <- mac %>% 
   mutate(affiliation_countries = str_extract_all((affiliation), pattern)) %>%
   unnest(affiliation_countries, keep_empty = TRUE)
 
 ## clean up values
 
-missing$affiliation_countries[missing$affiliation_countries == "Russi"] <- "Russia"
-missing$affiliation_countries[missing$affiliation_countries == "Masar"] <- "Czech Republic"
-missing$affiliation_countries[missing$affiliation_countries == "Norwi"] <- "United Kingdom"
-missing$affiliation_countries[missing$affiliation_countries == "Republic of Korea"] <- "South Korea"
+mac$affiliation_countries[mac$affiliation_countries == "Russi"] <- "Russia"
+mac$affiliation_countries[mac$affiliation_countries == "Masar"] <- "Czech Republic"
+mac$affiliation_countries[mac$affiliation_countries == "Norwi"] <- "United Kingdom"
+mac$affiliation_countries[mac$affiliation_countries == "Republic of Korea"] <- "Korea"
 
 ## concatenate and dedup
 
-missing <- missing %>% 
+mac <- mac %>% 
   group_by(ID) %>% 
     mutate(affiliation_countries = paste(affiliation_countries, collapse=", "))
-missing <- unique(missing)
-missing$affiliation_countries[missing$affiliation_countries == "NA"] <- NA
+mac<- unique(mac)
+mac$affiliation_countries[mac$affiliation_countries == "NA"] <- NA
 
 ## recombine with rest of update
 
-new2 <- left_join(new, missing, by = "ID", suffix = c(".new", ".y"))
-new2$corrected_affiliation_countries <- coalesce(new2$affiliation_countries.y, new2$affiliation_countries.new)
-new3 <- new2 %>% select(-ends_with(".y"))
-new4 <- select(new3,-20) ## remove original affiliation_countries column 
-names(new4) <- names(new4) %>% gsub(".new", "", .) 
-new <- new4
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == ""] <- NA
-
-new <- new %>%
-  mutate(corrected_affiliation_countries = strsplit(corrected_affiliation_countries, ", ")) %>%
-  unnest(corrected_affiliation_countries) %>%
-  group_by(corrected_affiliation_countries) 
+new2 <- left_join(new, mac, by = "ID", suffix = c(".new", ".y"))
+new2$aug_affiliation_countries <- coalesce(new2$affiliation_countries.y, new2$affiliation_countries.new)
+new2 <- new2 %>% select(-ends_with(".y"))
+new2 <- select(new2,-20) ## remove original affiliation_countries column 
+names(new2) <- names(new2) %>% gsub(".new", "", .) 
+new2$aug_affiliation_countries[new2$aug_affiliation_countries == ""] <- NA
+new_c <- new2
 
 ## clean up combined 
 
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == "UK"] <- "United Kingdom"
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == "Czechia"] <- "Czech Republic"
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == "Guadeloupe"] <- "France"
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == "Korea"] <- "South Korea"
-new$corrected_affiliation_countries[new$corrected_affiliation_countries == "Jersey"] <- "USA"
+new_c <- new_c %>%
+  mutate(aug_affiliation_countries = strsplit(aug_affiliation_countries, ", ")) %>%
+  unnest(aug_affiliation_countries) 
 
-new <- ungroup(new)
+new_c$aug_affiliation_countries[new_c$aug_affiliation_countries == "Guadeloupe"] <- "France"
+new_c$aug_affiliation_countries[new_c$aug_affiliation_countries == "Jersey"] <- "USA"
+
+## re-list countries by ID
+
+new_c <- new_c %>% 
+  group_by(ID) %>%
+  mutate(aug_affiliation_countries = paste0(aug_affiliation_countries, collapse = ", ")) 
+
+new_c <- unique(new_c)
+new_c <- ungroup(new_c)
 
 ##===============================================================##
 ######### PART 3: Augment Country from URL Geo-coordinates  ####### 
 ##===============================================================##
 
-## lat/long revered for some geocoordinates
+## Missing URL Countries: lat/long reversed for some geocoordinates
 
 muc <-filter(new, new$extracted_url_country == "")
 muc <-filter(muc, muc$extracted_url_coordinates != "")
@@ -122,14 +126,15 @@ muc <- select(muc, 1, 10, 11, 12, 13)
 ## remove errant "," at starts
 muc$extracted_url_coordinates <- sub("^[^\\(]+", "", muc$extracted_url_coordinates)
 muc <- muc %>% mutate_all(na_if,"")
-## filter again
-muc <-filter(muc, muc$extracted_url_coordinates != "")
+## filter again to remove emptied values
+muc <- filter(muc, muc$extracted_url_coordinates != "")
 muc <- separate(data = muc, col = extracted_url_coordinates, into = c("left", "right", "left2", "right2"), sep = ",")
-muc <- select(muc, -"left2", -"right2") ## just doing first url
+## just retrieving for first url
+muc <- select(muc, -"left2", -"right2") 
 muc$left = as.character(gsub("\\(", "", muc$left))
 muc$right = as.character(gsub("\\)", "", muc$right))
-# muc$left2 = as.character(gsub(" \\(", "", muc$left2)) ## if doing second
-# muc$right2 = as.character(gsub("\\)", "", muc$right2))
+# muc$left2 = as.character(gsub(" \\(", "", muc$left2)) ## if doing second url
+# muc$right2 = as.character(gsub("\\)", "", muc$right2)) ## if doing second url
 muc <- muc %>% mutate(across(where(is.character), str_trim))
 
 t  <- NULL;
@@ -146,30 +151,36 @@ t <- as.data.frame(t)
 t$extracted_url_country = as.character(gsub(":.*", "", t$extracted_url_country))
 
 ## recombine with rest of update
-new6 <- left_join(new, t, by = "ID", suffix = c(".new", ".y"))
-new6$corrected_extracted_url_country <- coalesce(new6$extracted_url_country.y, new6$extracted_url_country.new)
-new6$corrected_extracted_url_country[new6$corrected_extracted_url_country == ""] <- NA
-new7 <- new6 %>% select(-ends_with(".y"))
-new8 <- select(new7,1:11, 13, 23, 14:20)
-new <- new8
+new3 <- left_join(new_c, t, by = "ID", suffix = c(".new_c", ".y"))
+new3$aug_extracted_url_country <- coalesce(new3$extracted_url_country.y, new3$extracted_url_country.new_c)
+new3$aug_extracted_url_country[new3$aug_extracted_url_country == ""] <- NA
+new3 <- new3 %>% select(-ends_with(".y"))
+new3 <- select(new3,1:11, 13, 23, 14:20)
+new_c <- new3
 
-##==================================================##
-######### PART 4: Analyze Newly ID'd Resources ####### 
-##==================================================##
+write.csv(new_c, "new_biodata_resources_2024_augmented.csv", row.names = FALSE)
+
+##=============================================================##
+######### PART 4: Plot Countries for Newly ID'd Resources ####### 
+##=============================================================##
 
 ## get location columns
 
-nlocations <- new %>%
-  select(corrected_extracted_url_country,
+nlocations <- new_c %>%
+  select(aug_extracted_url_country,
     extracted_url_coordinates,
-    corrected_affiliation_countries)
+    aug_affiliation_countries)
 
 ## Author affiliations 
 
 author_country_counts <- nlocations %>%
-  select(corrected_affiliation_countries) %>%
-  rename(country = corrected_affiliation_countries) %>%
+  select(aug_affiliation_countries) %>%
+  rename(country = aug_affiliation_countries) %>%
   na.omit()
+
+author_country_counts <- author_country_counts %>%
+  mutate(country = strsplit(country, ", ")) %>%
+  unnest(country) 
 
 author_country_counts <- author_country_counts %>%
   mutate(
@@ -206,9 +217,13 @@ author_plot
 ## URLs
 
 url_countries <- nlocations %>%
-  select(corrected_extracted_url_country) %>%
-  rename(country = corrected_extracted_url_country) %>%
+  select(aug_extracted_url_country) %>%
+  rename(country = aug_extracted_url_country) %>%
   na.omit()
+
+url_countries  <- url_countries %>%
+  mutate(country = strsplit(country, ", ")) %>%
+  unnest(country) 
 
 url_countries <- url_countries %>%
 mutate(
